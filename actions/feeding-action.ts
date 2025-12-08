a503c9f57b5c2ab7f5d5bdaa895b52c68fcd17c6
melatonina0015 <kwam0015@gmail.com>
2025-12-09 00:46:21 +0100

    Update logic for adding FeedingTasks

--- actions/feeding-action.ts
+++ actions/feeding-action.ts
@@ -3,20 +3,35 @@
 import * as z from "zod";
 import {createFeedingTask, toggleFeedingTask} from "@/services/feeding-service";
 import {revalidatePath} from "next/cache";
-import {State} from "@/actions/animal-actions";
 
 const today = new Date();
 today.setHours(0, 0, 0, 0);
 
+export type FeedingState = {
+    errors?: {
+        animalId?: string[];
+        dueDate?: string[];
+        foodType?: string[];
+        foodQuantity?: string[];
+    };
+    message?: string | null;
+};
+
 const feedingTaskValidation = z.object({
     animalId: z.string(),
-    dueDate: z.coerce.date().min(today, {message: "Data musi być w przyszłości."})
+    dueDate: z.coerce.date().min(today, {message: "Data musi być w przyszłości."}),
+    foodType: z.string(),
+    foodQuantity: z.preprocess((val) => (val === "" ? null : val),
+        z.coerce.number().positive().int().nullable().optional()
+    )
 })
 
-export const createFeedingTaskAction = async (prevState: State, formData: FormData) => {
+export const createFeedingTaskAction = async (prevState: FeedingState, formData: FormData) => {
     const rawFormData = {
         animalId: formData.get("animalId"),
-        dueDate: formData.get("dueDate")
+        dueDate: formData.get("dueDate"),
+        foodType: formData.get("foodType"),
+        foodQuantity: formData.get("foodQuantity")
     }
 
     const validationResult = feedingTaskValidation.safeParse(rawFormData);
