3a0f3d6733162f1770c2e896997332d703907d6b
melatonina0015 <kwam0015@gmail.com>
2025-12-08 21:01:26 +0100

    Optimistic FeedingTask update

--- components/feeding-item.tsx
+++ components/feeding-item.tsx
@@ -1,20 +1,38 @@
 'use client'
 
 import {toggleFeedingTaskAction} from "@/actions/feeding-action";
+import {startTransition, useOptimistic} from "react";
 
-export const FeedingItem = ({task, animalId}) => {
+export const FeedingItem = ({task, animalId}: { task: any, animalId: string }) => {
+    const [optimisticToggle, addOptimisticToggle] = useOptimistic(
+        task,
+        (currState, newValue) => {
+            return {...currState, isDone: newValue};
+        }
+    );
+
+    const handleToggle = async () => {
+        const newValue = !optimisticToggle.isDone;
+
+        startTransition(() => {
+            addOptimisticToggle(newValue);
+        })
+
+        await toggleFeedingTaskAction(task.id, newValue, animalId);
+    }
+
     return (
         <li
             className={`p-3 rounded border flex justify-between items-center ${
-                task.isDone ? "bg-gray-100 text-gray-400" : "bg-white"
+                optimisticToggle.isDone ? "bg-gray-100 text-gray-400" : "bg-white"
             }`}
         >
-            <span>{task.dueDate.toLocaleDateString()}</span>
+            <span>{optimisticToggle.dueDate.toLocaleDateString()}</span>
             <span className="text-sm">
-                    {task.isDone ? "Wykonane" : "Do zrobienia"}
+                    {optimisticToggle.isDone ? "Wykonane" : "Do zrobienia"}
             </span>
-            <input type="checkbox" checked={task.isDone}
-                   onChange={() => toggleFeedingTaskAction(task.id, !task.isDone, animalId)}/>
+            <input type="checkbox" checked={optimisticToggle.isDone}
+                   onChange={handleToggle}/>
         </li>
     )
 }
