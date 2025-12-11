223448aceae157b319282dc12075a42976c8f8b3
melatonina0015 <kwam0015@gmail.com>
2025-12-11 19:02:46 +0100

    Add styling for Overdue Tasks

--- components/feeding-item.tsx
+++ components/feeding-item.tsx
@@ -4,7 +4,6 @@ import {toggleFeedingTaskAction} from "@/actions/feeding-action";
 import {startTransition, useOptimistic} from "react";
 import {FeedingTask} from "@/types";
 
-
 export const FeedingItem = ({task, animalId}: { task: FeedingTask, animalId: string }) => {
     const [optimisticToggle, addOptimisticToggle] = useOptimistic<FeedingTask, boolean>(
         task,
@@ -23,11 +22,27 @@ export const FeedingItem = ({task, animalId}: { task: FeedingTask, animalId: str
         await toggleFeedingTaskAction(task.id, newValue, animalId);
     }
 
+    const isTaskOverdue = () => {
+        const today = (new Date()).setHours(0, 0, 0, 0);
+        const taskDateObj = new Date(optimisticToggle.dueDate);
+        const taskDate = taskDateObj.setHours(0, 0, 0, 0);
+
+        return taskDate < today;
+    }
+
+    const isOverdue = isTaskOverdue() && !optimisticToggle.isDone;
+
+    const liClassList = `p-3 rounded border flex justify-between items-center w-full ${
+        optimisticToggle.isDone
+            ? "bg-gray-100 text-gray-400"
+            : isOverdue
+                ? "border-red-500 bg-red-200"
+                : "border"
+    }`
+
     return (
         <li
-            className={`p-3 rounded border flex justify-between items-center w-full ${
-                optimisticToggle.isDone ? "bg-gray-100 text-gray-400" : "bg-white"
-            }`}
+            className={liClassList}
         >
             <span>{optimisticToggle.dueDate.toLocaleDateString()}</span>
             <div className="flex gap-2">
